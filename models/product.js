ab8a4bc6e55e8621a519c5c81019830dba663415
umed tolibi <tolibiumed@gmail.com>
2018-12-16 00:22:58 -0500

    devided the logic of the app using MVP pattern

---
+++ models/product.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, filecontent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(filecontent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => console.log(err));
+    });
+  }
+
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, filecontent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(filecontent));
+    });
+  }
+};
